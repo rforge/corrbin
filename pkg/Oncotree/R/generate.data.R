05bfa906d77ce465caf76ddc3aa2a21ad91fae44
anikoszabo <anikoszabo@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-04-12 21:42:33 +0000

    Added alternative data generation methods
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/corrbin@67 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Oncotree/R/generate.data.R
+++ pkg/Oncotree/R/generate.data.R
@@ -1,13 +1,56 @@
 "generate.data" <-
 function(N, otree, with.errors=TRUE,
-          edge.weights=if (with.errors) "estimated" else "observed"){
-	distr <- distribution.oncotree(otree, with.probs=TRUE, with.errors=with.errors,
-	      edge.weights=edge.weights)
-	epos <- otree$eps["epos"]
-	eneg <- otree$eps["eneg"]
-	ran.idx <- sample(1:nrow(distr), size=N, prob=distr$Prob, replace=TRUE)
-	ran.data <- distr[ran.idx, 2:otree$nmut]
-	rownames(ran.data) <- 1:N
+         edge.weights=if (with.errors) "estimated" else "observed",
+         method=c("D1","D2","S")){
+  
+  method <- match.arg(method)
+  edge.weights <- match.arg(edge.weights)
+  if (!with.errors && method=="D2") method <- "D1"  #D2 only makes sense with errors
+  
+  if (method=="D1"){
+    #calculate full joint distribution and sample from it
+    distr <- distribution.oncotree(otree, with.probs=TRUE, with.errors=with.errors,
+                                   edge.weights=edge.weights)
+    ran.idx <- sample(1:nrow(distr), size=N, prob=distr$Prob, replace=TRUE)
+    ran.data <- distr[ran.idx, 2:otree$nmut]
+    rownames(ran.data) <- 1:N    
+  }
+  
+  if (method=="D2"){
+    #calculate joint distribution without errors, then "corrupt" the sample
+    distr <- distribution.oncotree(otree, with.probs=TRUE, with.errors=FALSE,
+                                   edge.weights=edge.weights)
+    ran.idx <- sample(1:nrow(distr), size=N, prob=distr$Prob, replace=TRUE)
+    ran.data0 <- distr[ran.idx, 2:otree$nmut]
+    rownames(ran.data0) <- 1:N    
+    
+    epos <- otree$eps["epos"]
+    eneg <- otree$eps["eneg"]
+    ran.data <- matrix(rbinom(prod(dim(ran.data0)), size=1, p=ifelse(ran.data0==0,epos,1-eneg)),
+                       nrow=nrow(ran.data0), ncol=ncol(ran.data0), 
+                       dimnames=dimnames(ran.data0))
+  }
+  
+  if (method=="S"){
+    #simulate based on definition, then "corrupt" the sample
+    plotinfo <- build.plot(otree$parent) #need level info
+    maxLev <- max(plotinfo$level)
+    level.list <- split(otree$parent$child, plotinfo$level)
+    ran.data <- matrix(NA, nrow=N, ncol=otree$nmut)
+    colnames(ran.data) <- otree$parent$child
+    w <- if (edge.weights=="observed") otree$parent$obs.weight else otree$parent$est.weight
+    ran.data[,1] <- 1  #Root is always present
+    
+    for (lev in 2:maxLev){
+      events <- level.list[[lev]]
+      for (ev in events){
+        ev.parent <- otree$parent$parent[otree$parent$child==ev]
+        transition <- rbinom(N, size=1, p=w[ev])
+        ran.data[,ev] <- ran.data[,ev.parent] * transition #only happens, if parent already occurred
+      }    
+    }   
+    ran.data <- ran.data[,-1]  #remove root
+  }
 	ran.data
 }
 
